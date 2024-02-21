66e386a6a42351453a71609f1ba76388c1ef5fff
Vince Guan <vinceguan@gmail.com>
2024-02-21 15:46:13 -0800

    more setting changes

--- django_server/django_server/settings_prod.py
+++ django_server/django_server/settings_prod.py
@@ -6,7 +6,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_COOKIE_DOMAIN = ['.awsapprunner.com']
+CSRF_COOKIE_DOMAIN = ['8wew43ppur.us-east-1.awsapprunner.com', '8wew43ppur.us-east-1.awsapprunner.com']
 CSRF_TRUSTED_ORIGINS = ['.awsapprunner.com']
 CSRF_COOKIE_PATH = '/'
 CSRF_COOKIE_SAMESITE = 'None'  
@@ -14,7 +14,7 @@ CSRF_COOKIE_HTTPONLY = True
 CSRF_USE_SESSIONS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS=[
-    "http://localhost:3000",
+    "https://8wew43ppur.us-east-1.awsapprunner.com",
     '.awsapprunner.com'
 ]
 CORS_ALLOW_CREDENTIALS=True
