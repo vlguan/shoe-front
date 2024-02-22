2ce88ae35b91de1ef9a136db13b36c805adff554
Vince Guan <vinceguan@gmail.com>
2024-02-22 13:43:04 -0800

    new trusted domains in prod

--- django_server/django_server/settings_prod.py
+++ django_server/django_server/settings_prod.py
@@ -1,13 +1,13 @@
 from .settings import *
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', ".awsapprunner.com",]
+ALLOWED_HOSTS = ['0.0.0.0', ".awsapprunner.com",'.littlefeetca.com']
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_COOKIE_DOMAIN = ['8wew43ppur.us-east-1.awsapprunner.com', '8wew43ppur.us-east-1.awsapprunner.com']
-CSRF_TRUSTED_ORIGINS = ['.awsapprunner.com']
+CSRF_COOKIE_DOMAIN = ['8wew43ppur.us-east-1.awsapprunner.com', '8wew43ppur.us-east-1.awsapprunner.com', '.littlefeetca.com']
+CSRF_TRUSTED_ORIGINS = ['.awsapprunner.com','.littlefeetca.com']
 CSRF_COOKIE_PATH = '/'
 CSRF_COOKIE_SAMESITE = 'None'  
 CSRF_COOKIE_HTTPONLY = True
@@ -15,6 +15,7 @@ CSRF_USE_SESSIONS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS=[
     "https://8wew43ppur.us-east-1.awsapprunner.com",
-    '.awsapprunner.com'
+    '.awsapprunner.com',
+    '.littlefeetca.com'
 ]
 CORS_ALLOW_CREDENTIALS=True
